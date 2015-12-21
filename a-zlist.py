db8bcef324fffaa4c78d043d2daed0e969ecca94
Ethan Fenichel <EthanFenichel@gmail.com>
2015-12-21 15:04:54 -0500

    added none check and updated for the input file

--- a-zlist.py
+++ a-zlist.py
@@ -9,8 +9,14 @@ root = tkinter.Tk()
 root.withdraw()
 
 baseURL = 'http://hx8vv5bf7j.search.serialssolutions.com/?V=1.0&L=HX8VV5BF7J&S=I_M&C='
+nothingFound = '0 RECORDS RETRIEVED FOR THE SEARCH'
 
 def checkISSN(vendorName, issn):
+    """
+    if the input has None, we default to assuming we've found the file, if we return a 0 result, we'll return no results
+    we check the vendor name - it must be equal to "None" (cap-sensitive) in order to trigger the no vendor check
+    otherwise, it will search for a match for the provided vendor name
+    """
 
     # issn = '1544-0737'
     # vendorName = 'Sage'
@@ -21,9 +27,16 @@ def checkISSN(vendorName, issn):
 
     vendorFound = False
 
-    for l in httpList:
-        if vendorName.upper() in l.decode().upper():
-            vendorFound = True
+    if vendorName != "None":
+        for l in httpList:
+            if vendorName.upper() in l.decode().upper():
+                vendorFound = True
+
+    if vendorName == 'None':
+        vendorFound = True
+        for l in httpList:
+            if nothingFound in l.decode().upper():
+                vendorFound = False
 
     return vendorFound
 
@@ -38,11 +51,12 @@ def readFile():
     alephbib, title, vendor, issn, any other columns are fine but won't be read
     """
     input('press any key to choose the input csv file...')
-    print('thanks! got it!\nrunning...')
 
     from tkinter import filedialog
     csvPath = tkinter.filedialog.askopenfile()
     fileName = csvPath.name
+
+    print('thanks! got it!\nrunning...')
 
 
     checkList = []
